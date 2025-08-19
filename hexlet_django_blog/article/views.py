8fd78f9b2696f3d62d111d98dca8efcc4da513ff
Olga <o.gavrikova877@gmail.com>
2025-08-19 16:55:53 +0400

    base.html

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+# def index(request):
+#     return HttpResponse("article")
+
 def index(request):
-    return HttpResponse("article")
+    return render(
+        request,
+        "articles/index.html",
+        context={
+            "name": "Article",
+        },
+    )
